aa29ce473e7cb6cd36160faeeb3e22c707333c6a
Ben Horkley <ben.horkley@gmail.com>
2022-01-26 20:46:37 -0500

    fix CK field order

--- converter.py
+++ converter.py
@@ -10,13 +10,14 @@ sets_req.raise_for_status()
 # https://scryfall.com/docs/api/sets
 sets = {x["code"].upper(): x for x in sets_req.json()["data"]}
 
+
 def set_code_to_name(code: str) -> str:
     # TODO: deal with sets that providers don't call by name (e.g. Promotional)
     return sets[code]["name"]
 
 
 class CardMetadata:
-    def __init__(self, quantity: int, name: str, set_code: str, is_foil: bool=False):
+    def __init__(self, quantity: int, name: str, set_code: str, is_foil: bool = False):
         self.quantity = quantity
         self.name = name
         self.set_code = set_code
@@ -24,23 +25,23 @@ class CardMetadata:
         self.set_name = set_code_to_name(self.set_code)
 
     def cardkingdom_csv_data(self) -> List:
-        return [self.quantity, self.name, self.set_name, self.is_foil]
+        return [self.quantity, self.name, self.is_foil, self.set_name]
 
 
-TCGPLAYER_REGEX = r'(\d+) (.*) \[(.*)\]'
+TCGPLAYER_REGEX = r"(\d+) (.*) \[(.*)\]"
 
 if __name__ == "__main__":
     cards = []
 
     # For now, expect TCGplayer collection output format:
     # 4 Polluted Delta [ONS]
-    with open("input.txt", 'r') as f:
+    with open("input.txt", "r") as f:
         while line := f.readline():
             fields = re.match(TCGPLAYER_REGEX, line.strip())
             quantity, name, code = fields.groups()
             cards.append(CardMetadata(quantity=quantity, name=name, set_code=code))
 
-    with open("output.csv", 'w', newline='') as f:
+    with open("output.csv", "w", newline="") as f:
         w = csv.writer(f)
         for card in cards:
             w.writerow(card.cardkingdom_csv_data())
